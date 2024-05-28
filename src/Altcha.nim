4490fe48fc0d39bf0498d08e119c02daf6dbdd3d
21DP4KLogi <103188972+21DP4KLogi@users.noreply.github.com>
2024-05-28 16:40:41 +0300

    Implemented challenge generation
    Hopefully works, should also add more comments.

--- src/Altcha.nim
+++ src/Altcha.nim
@@ -1,8 +1,41 @@
 import jester
+import std/[sysrand, strutils, json]
+import nimcrypto/[sha2, hmac]
+
+const complexity: uint = 1_000_000
+const hmacKey = "password123"
 
 routes:
-  get "/newChallenge":
-    resp Http200
+  get "/new":
+    
+    var randomBytes: seq[byte]
 
-  post "/submitChallenge":
+    # Generating salt
+    randomBytes = urandom(5)
+    var salt: string
+    for i in randomBytes:
+      salt.add i.toHex
+    
+    # Generating secret number
+    randomBytes = urandom(8)
+    var randomNumber: uint
+    for i in 0..(randomBytes.len - 1):
+      randomNumber += randomBytes[i]
+      randomNumber = randomNumber shl 8
+    let secretNumber = randomNumber mod complexity
+
+    # Generating challenge hash
+    let challenge = $sha256.digest(salt & $secretNumber)
+
+    # Generating server signature
+    let signature = $sha256.hmac(hmacKey, challenge)
+
+    resp(%* {
+      "algorithm": "SHA-256",
+      "challenge": challenge,
+      "salt": salt,
+      "signature": signature,
+    })
+
+  post "/submit":
     resp Http200
